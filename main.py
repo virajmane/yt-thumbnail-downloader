ce852e6027070ff2b17545965ce9305ff2cdf6dc
Viraj <62030782+virajmane@users.noreply.github.com>
2020-11-12 01:11:36 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import re
+url = # Any Youtube Here URL
+exp = "^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*"
+s = re.findall(exp,url)[0][-1]
+thumbnail = f"https://i.ytimg.com/vi/{s}/maxresdefault.jpg"
+print(thumbnail)
